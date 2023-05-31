4b01ecadd6a48e768ab458a6acfd52cb07bc93df
LoneDev6 <leoxy98@gmail.com>
2023-05-31 14:53:20 +0200

    Added Campfire events

---
+++ src/main/java/dev/lone/itemsadder/api/Events/campfire/CampfireLitEvent.java
@@ -0,0 +1,54 @@
+package dev.lone.itemsadder.api.Events.campfire;
+
+import org.bukkit.block.Campfire;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("unused")
+public class CampfireLitEvent extends Event implements Cancellable
+{
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    boolean isCancelled;
+    final Campfire campfire;
+    final Player player;
+
+    public CampfireLitEvent(@NotNull Campfire campfire,
+                            @NotNull Player player)
+    {
+        this.campfire = campfire;
+        this.player = player;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers()
+    {
+        return HANDLERS;
+    }
+
+    @Override
+    public boolean isCancelled()
+    {
+        return this.isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b)
+    {
+        this.isCancelled = b;
+    }
+
+    public Campfire getCampfire()
+    {
+        return campfire;
+    }
+
+    public Player getPlayer()
+    {
+        return player;
+    }
+}
---
+++ src/main/java/dev/lone/itemsadder/api/Events/campfire/CampfireRemoveItemEvent.java
@@ -0,0 +1,68 @@
+package dev.lone.itemsadder.api.Events.campfire;
+
+import org.bukkit.block.Campfire;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("unused")
+public class CampfireRemoveItemEvent extends Event implements Cancellable
+{
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    boolean isCancelled;
+    final Player player;
+    final ItemStack item;
+    Campfire campfire;
+
+    public CampfireRemoveItemEvent(@NotNull Campfire campfire,
+                                   @NotNull ItemStack item,
+                                   @NotNull Player player)
+    {
+        this.campfire = campfire;
+        this.item = item;
+        this.player = player;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers()
+    {
+        return HANDLERS;
+    }
+
+    @Override
+    public boolean isCancelled()
+    {
+        return this.isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b)
+    {
+        this.isCancelled = b;
+    }
+
+    public static HandlerList getHandlerList()
+    {
+        return HANDLERS;
+    }
+
+    public Player getPlayer()
+    {
+        return player;
+    }
+
+    public ItemStack getItem()
+    {
+        return item;
+    }
+
+    public Campfire getCampfire()
+    {
+        return campfire;
+    }
+}
